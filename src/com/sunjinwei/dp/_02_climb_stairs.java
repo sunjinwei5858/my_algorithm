ddb00368e549884b3d46c29b529353d355ad10ff
sunjinwei <sunjinwei5828@163.com>
2021-01-02 18:37:54 +0800

    爬楼梯和完全背包的重新思考

--- src/com/sunjinwei/dp/_02_climb_stairs.java
+++ src/com/sunjinwei/dp/_02_climb_stairs.java
@@ -30,4 +30,26 @@ public class _02_climb_stairs {
         return current;
     }
 
+    /**
+     * 动态规划 一维数组求解
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs_02(int n) {
+        // 这里必须是小于等于2 不是1 因为dp[2]=2 dp[0]+dp[1]=1
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
 }
