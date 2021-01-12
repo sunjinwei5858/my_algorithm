c19dce55fda9cab23e713008a0766cb88d561886
sunjinwei <sunjinwei5828@163.com>
2021-01-12 17:40:31 +0800

    二叉树的前序遍历：递归+栈实现

---
+++ src/com/sunjinwei/_tree/_02_pre_order.java
@@ -0,0 +1,67 @@
+package com.sunjinwei._tree;
+
+import com.sunjinwei.domain.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 前序遍历 先根节点 左子树 右子树
+ */
+public class _02_pre_order {
+
+    private List<Integer> result = new ArrayList<>();
+
+    /**
+     * 递归实现
+     *
+     * @param node
+     * @return
+     */
+    public List<Integer> preOrder(TreeNode node) {
+
+        if (node == null) {
+            return result;
+        }
+        result.add(node.val);
+        if (node.left != null) {
+            preOrder(node.left);
+        }
+        if (node.right != null) {
+            preOrder(node.right);
+        }
+        return result;
+    }
+
+    /**
+     * 栈实现
+     *
+     * @param node
+     * @return
+     */
+    public List<Integer> preOrder_02(TreeNode node) {
+
+        if (node == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.empty()) {
+            // 1根节点出栈
+            TreeNode pop = stack.pop();
+            result.add(pop.val);
+            // 2右子树进栈
+            if (pop.right != null) {
+                stack.push(pop.right);
+            }
+            // 3左子树进栈
+            if (pop.left != null) {
+                stack.push(pop.left);
+            }
+        }
+        return result;
+    }
+
+
+}
