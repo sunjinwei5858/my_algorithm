cfb380e1465181f96aaaa72a30269980bc512e21
sunjinwei <sunjinwei5828@163.com>
2021-01-12 17:41:37 +0800

    全排列，回溯算法，模板使用Deque队列作为泛型

--- src/com/sunjinwei/bfs_dfs_huisu/_02_quan_pailie.java
+++ src/com/sunjinwei/bfs_dfs_huisu/_02_quan_pailie.java
@@ -1,6 +1,7 @@
 package com.sunjinwei.bfs_dfs_huisu;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,33 +16,33 @@ public class _02_quan_pailie {
     List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> list = new LinkedList<>();
+        // 使用队列 移除最后一个元素比较方便
+        Deque<Integer> list = new LinkedList<>();
         huisu(nums, list);
         return result;
     }
 
-    private void huisu(int[] nums, List<Integer> list) {
+    private void huisu(int[] nums, Deque<Integer> list) {
         // 递归终止条件
         if (nums.length == list.size()) {
             /**
-             * 在 Java 中，参数传递是 值传递，对象类型变量在传参的过程中，复制的是变量的地址。
-             * 这些地址被添加到 res 变量，但实际上指向的是同一块内存地址，因此我们会看到 6 个空的列表对象。
+             * 在java中，参数传递是值传递，对象类型变量在传参的过程中，复制的是变量的地址。
+             * 这些地址被添加到res变量，但实际上指向的是同一块内存地址，因此我们会看到6个空的列表对象。
              * 解决的方法很简单，在 res.add(path); 这里做一次拷贝即可。
              *
              */
-            System.out.println("===="+list.toString());
             result.add(new ArrayList<>(list));
             return;
         }
         for (int num : nums) {
-            // 做选择
+            // 做选择 剪枝
             if (list.contains(num)) {
                 continue;
             }
             list.add(num);
             huisu(nums, list);
             // 撤销选择 撤销的是list的最后一个元素
-            list.remove(list.size() - 1);
+            list.removeLast();
         }
     }
 
