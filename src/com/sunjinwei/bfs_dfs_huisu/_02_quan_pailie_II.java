1e2949decbe43a0081c8ee2a5ea10eddfff0d95a
sunjinwei <sunjinwei5828@163.com>
2021-01-17 18:37:50 +0800

    全排列的变种，力扣47，给定一个可包含重复数字的序列nums，按任意顺序 返回所有不重复的全排列，自己终于又AC了一道题

---
+++ src/com/sunjinwei/bfs_dfs_huisu/_02_quan_pailie_II.java
@@ -0,0 +1,76 @@
+package com.sunjinwei.bfs_dfs_huisu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 全排列的变种47：给定一个可包含重复数字的序列nums，按任意顺序 返回所有不重复的全排列
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * 示例 2：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ */
+public class _02_quan_pailie_II {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        // 声明布尔数组 记录元素的使用状态
+        boolean[] used = new boolean[nums.length];
+
+        huisu_01(nums, list, used);
+
+        return result;
+    }
+
+    /**
+     * 自己的方法：不重复的数列 直接使用contains判断
+     * 执行用时：825 ms, 在所有 Java 提交中击败了5.06%的用户
+     * 内存消耗：38.8 MB, 在所有 Java 提交中击败了96.71%的用户
+     *
+     * @param nums
+     * @param list
+     * @param used
+     */
+    private void huisu_01(int[] nums, LinkedList<Integer> list, boolean[] used) {
+        // 结束条件：还需要加上的条件是 不重复 直接使用contains
+        if (list.size() == nums.length && !result.contains(new ArrayList<>(list))) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 如果没有使用
+            if (!used[i]) {
+                // 做选择
+                used[i] = true;
+                list.add(nums[i]);
+                // 递归
+                huisu_01(nums, list, used);
+                // 撤销选择
+                used[i] = false;
+                list.removeLast();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[]{1, 2, 1};
+        _02_quan_pailie_II quanPailie = new _02_quan_pailie_II();
+        List<List<Integer>> permute = quanPailie.permuteUnique(ints);
+        System.out.println(permute.toString());
+
+
+    }
+
+}
