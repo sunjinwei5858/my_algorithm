772b28af2a3002a2db173cde3d4b66eef5123010
sunjinwei <sunjinwei5828@163.com>
2021-01-19 08:53:26 +0800

    子集问题：题解的方法，其实就是树的前序遍历，一定要画出递归树!!!，要么是所有节点，要么是叶子节点!!!!

--- src/com/sunjinwei/bfs_dfs_huisu/_05_ziji_I.java
+++ src/com/sunjinwei/bfs_dfs_huisu/_05_ziji_I.java
@@ -72,7 +72,7 @@ public class _05_ziji_I {
     }
 
     /**
-     * 方法2：直接暴力对结果集判重 不需要排序和boolean记录状态
+     * 自己的方法2：直接暴力对结果集判重 不需要排序和boolean记录状态
      * 执行用时：8 ms, 在所有 Java 提交中击败了12.95%的用户
      * 内存消耗：38.5 MB, 在所有 Java 提交中击败了83.14%的用户 【加了Arrays.sort排序内存】
      * <p>
@@ -115,12 +115,46 @@ public class _05_ziji_I {
         }
     }
 
+    /**
+     * 题解的方法：和自己的对比 因为自己没有画出递归树 所以无法知道这个子集 其实可以当作树的前序遍历
+     * <p>
+     * 执行用时：1 ms, 在所有 Java 提交中击败了84.77%的用户
+     * 内存消耗：38.7 MB, 在所有 Java 提交中击败了58.03%的用户
+     *
+     * @param nums
+     */
+    public List<List<Integer>> subsets_03(int[] nums) {
+        // 鲁棒性1
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        // 声明路径集合
+        LinkedList<Integer> path = new LinkedList<>();
+        // 递归回溯
+        subsets_help_03(nums, path, 0);
+        return result;
+    }
+
+    private void subsets_help_03(int[] nums, LinkedList<Integer> path, int index) {
+        // 其实子集就是树的前序遍历
+        result.add(new ArrayList<>(path));
+        for (int i = index; i < nums.length; i++) {
+            // 做选择
+            path.add(nums[i]);
+            // 递归
+            subsets_help_03(nums, path, i + 1);
+            // 撤销选择
+            path.removeLast();
+        }
+
+    }
+
 
     public static void main(String[] args) {
         _05_ziji_I ziji_i = new _05_ziji_I();
         int[] arr = new int[]{1, 2, 3};
 
-        List<List<Integer>> subsets = ziji_i.subsets_02(arr);
+        List<List<Integer>> subsets = ziji_i.subsets_03(arr);
         for (List<Integer> subset : subsets) {
             System.out.println(subset.toString());
         }
