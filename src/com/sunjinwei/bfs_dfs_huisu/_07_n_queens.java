c22741c0725c1e43124518b32eb7f97d4be62cf2
sunjinwei <sunjinwei5828@163.com>
2021-01-23 10:33:52 +0800

    n皇后的题解思路，n皇后非常综合，其实思路不难，要明白各种遍历方式

--- src/com/sunjinwei/bfs_dfs_huisu/_07_n_queens.java
+++ src/com/sunjinwei/bfs_dfs_huisu/_07_n_queens.java
@@ -1,6 +1,7 @@
 package com.sunjinwei.bfs_dfs_huisu;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,59 +19,76 @@ public class _07_n_queens {
 
     public List<List<String>> solveNQueens(int n) {
 
-        LinkedList<String> path = new LinkedList<>();
-        String[][] visited = new String[n][n];
-        // 初始化二维数组
+        List<char[]> board = new LinkedList<>();
+        // 初始化
         for (int i = 0; i < n; i++) {
-            visited[i][i] = ".";
+            // 创建char数组
+            char[] arr = new char[n];
+            Arrays.fill(arr, '.');
+            board.add(arr);
         }
-        solveNQueens_help(path, n, visited);
+        solveNQueens_help(board, 0);
         return result;
     }
 
-    private void solveNQueens_help(LinkedList<String> path, int n, String[][] visited) {
+    /**
+     * 从第一行开始回溯
+     *
+     * @param board
+     * @param row
+     */
+    private void solveNQueens_help(List<char[]> board, int row) {
 
-        if (path.size() == n) {
-            result.add(new ArrayList<>(path));
+        if (row == board.size()) {
+            // 把此时的二维数组添加到结果集
+            List<String> list = transform(board);
+            result.add(list);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!valid(visited, i, j)) {
-                    continue;
-                }
-                visited[i][j] = "Q";
-                solveNQueens_help(path, n, visited);
-                visited[i][j] = ".";
-                path.removeLast();
+        // n叉树的遍历 每一行都是从第一列 到第n列
+        for (int col = 0; col < board.size(); col++) {
+            // 剪枝
+            if (!valid(board, row, col)) {
+                continue;
             }
+            // 做选择
+            board.get(row)[col] = 'Q';
+            // 递归 进入下一行
+            solveNQueens_help(board, row + 1);
+            // 撤销选择
+            board.get(row)[col] = '.';
         }
     }
 
+    private List<String> transform(List<char[]> board) {
+        LinkedList<String> newBoard = new LinkedList<>();
+        for (char[] row : board) {
+            newBoard.add(new String(row));
+        }
+        return newBoard;
+    }
+
     /**
-     * @param visited
-     * @param row     行
-     * @param col     列
+     * @param board
+     * @param row   行
      * @return
      */
-    private boolean valid(String[][] visited, int row, int col) {
-        // 1左斜方向 行遍历倒序
-        for (int i = row, j = 0; i > j; i--, j++) {
-            if (Math.abs((i - j)) == Math.abs((row - col))) {
-                if (visited[i][j] == "Q") {
-                    return false;
-                }
-            }
-        }
-        // 2水平方向 列是递增的 行是一样的
-        for (int i = 0; i < col; i++) {
-            if (visited[row][i] == "Q") {
+    private boolean valid(List<char[]> board, int row, int col) {
+        // 1检查正上方是否有冲突 这里可以缩小范围 i<row即可 不需要遍历全部的行
+        for (int i = 0; i < row; i++) {
+            if (board.get(i)[col] == 'Q') {
                 return false;
             }
         }
-        // 3竖直方向 行是递增的
-        for (int i = 0; i < row; i++) {
-            if (visited[col][i] == "Q") {
+        // 2检查右斜方是否有冲突
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
+            if (board.get(i)[j] == 'Q') {
+                return false;
+            }
+        }
+        // 3检查左斜方是否有冲突
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board.get(i)[j] == 'Q') {
                 return false;
             }
         }
@@ -80,9 +98,7 @@ public class _07_n_queens {
     public static void main(String[] args) {
         _07_n_queens n_queens = new _07_n_queens();
         List<List<String>> lists = n_queens.solveNQueens(4);
-        for (List<String> list : lists) {
-            System.out.println(list.toString());
-        }
+        System.out.println(lists.toString());
 
 
     }
