6f42d18ee02b72868acc9807190bf1d6d48e3cef
sunjinwei <sunjinwei5828@163.com>
2021-01-26 14:39:06 +0800

    栈实现队列：剑指offer和力扣，一种非常好的思路：只有当stackPop为空的时候，才能将stackPush的数据压入stackPop中，所有方法都加上这个条件处理

--- src/com/sunjinwei/stack/_02_2stack_deque.java
+++ src/com/sunjinwei/stack/_02_2stack_deque.java
@@ -10,6 +10,11 @@ import java.util.Stack;
  * 请实现它的两个函数 appendTail 和 deleteHead ，
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
  * (若队列中没有元素，deleteHead 操作返回 -1 )
+ * <p>
+ * 需要满足的条件：
+ * 1 必须一次性将栈中的数据push到辅助栈
+ * 2 如果辅助栈不为空 不能push
+ * 以上两个条件 缺一不可 这道题目看似简单 其实没有那么简单
  */
 public class _02_2stack_deque {
 
@@ -24,24 +29,38 @@ public class _02_2stack_deque {
 
     /**
      * 队列尾部插入整数
+     *
      * @param value
      */
     public void appendTail(int value) {
         stackPush.push(value);
+        handler();
     }
 
     /**
      * 队列头部删除整数
+     *
      * @return
      */
     public int deleteHead() {
-        while (!stackPush.isEmpty() && stackPop.isEmpty()) {
-            stackPop.push(stackPush.pop());
-        }
+        handler();
         if (stackPop.isEmpty()) {
             return -1;
         }
         return stackPop.pop();
+    }
+
+    /**
+     * 抽取方法: 1。一次性压入 2。如果辅助pop栈不为空 不能进行压入
+     *
+     * @param
+     */
+    private void handler() {
+        if (stackPop.isEmpty()) {
+            while (!stackPush.isEmpty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -49,13 +68,13 @@ public class _02_2stack_deque {
         stack_deque.appendTail(1);
         stack_deque.appendTail(2);
 
-        System.out.println(stack_deque.deleteHead());
+        System.out.println(stack_deque.deleteHead()); // 1
 
         stack_deque.appendTail(3);
 
-        System.out.println(stack_deque.deleteHead());
-        System.out.println(stack_deque.deleteHead());
-        System.out.println(stack_deque.deleteHead());
+        System.out.println(stack_deque.deleteHead()); // 2
+        System.out.println(stack_deque.deleteHead()); // 3
+        System.out.println(stack_deque.deleteHead()); // -1
     }
 
 
