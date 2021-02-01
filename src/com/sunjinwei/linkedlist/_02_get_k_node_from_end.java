d8f936a52bf23f85138b3533a804c8904c782cbd
sunjinwei <sunjinwei5828@163.com>
2021-02-01 19:05:37 +0800

    链表：返回倒数第k个节点

---
+++ src/com/sunjinwei/linkedlist/_02_get_k_node_from_end.java
@@ -0,0 +1,18 @@
+package com.sunjinwei.linkedlist;
+
+import com.sunjinwei.domain.ListNode;
+
+
+/**
+ * 返回倒数第k个节点
+ */
+public class _02_get_k_node_from_end {
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        return null;
+
+    }
+
+
+}
