907d05bb37b0e4024026ddda5b9e8e93e1ed9898
sunjinwei <sunjinwei5828@163.com>
2021-02-02 18:52:06 +0800

    队列实现栈：使用一个队列!!!，妙哉!!!

--- src/com/sunjinwei/stack/_02_two_queue_stack_III.java
+++ src/com/sunjinwei/stack/_02_two_queue_stack_III.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 /**
  * 用队列实现栈，225
  * 方法三： 只用一个队列 在push的时候做文章
+ * 需要满足的条件：
+ * 队列的头部必须保证是刚push的元素，也就是push的元素要把他放在队列的头部位置，这里使用一个队列的话 那就是先将x进行push 然后把
  * <p>
  * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
  * 内存消耗：36.3 MB, 在所有 Java 提交中击败了33.37%的用户
@@ -28,7 +30,7 @@ public class _02_two_queue_stack_III {
         int size = queue.size();
         // 将该元素push到队列
         queue.offer(x);
-        // 将队列在push元素之前的都推出然后重新push 妙哉!!!
+        // 将队列的元素推出，然后重新放到队列 这样保证队列的元素在 当前元素 x的后面 妙哉!!!
         for (int i = 0; i < size; i++) {
             queue.offer(queue.poll());
         }
@@ -36,10 +38,6 @@ public class _02_two_queue_stack_III {
 
     /**
      * Removes the element on top of the stack and returns that element.
-     * 在pop方法 将队列的最后一个元素推出
-     * <p>
-     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
-     * 内存消耗：36.4 MB, 在所有 Java 提交中击败了22.30%的用户
      */
     public int pop() {
         return queue.poll();
@@ -47,8 +45,6 @@ public class _02_two_queue_stack_III {
 
     /**
      * Get the top element.
-     * 还需要对获取顶部元素进行改造 因为LinkedList 是双端队列 支持从头部/尾部 进行增删改查
-     * 自己感觉这并不是一个好办法，理论上应该是单向队列考虑
      */
     public int top() {
         return queue.peek();
