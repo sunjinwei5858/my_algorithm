cd894c181c8df1535b4751b700f6ef89337049c8
sunjinwei <sunjinwei5828@163.com>
2021-02-21 13:56:30 +0800

    二叉树：前序遍历，非递归的其他写法，总共有三种写法

--- src/com/sunjinwei/tree/_01_order_I.java
+++ src/com/sunjinwei/tree/_01_order_I.java
@@ -32,7 +32,7 @@ public class _01_order_I {
     }
 
     /**
-     * 栈实现
+     * 非递归：第一种写法 这种思想可以利用到后续遍历的两个栈解法上
      *
      * @param node
      * @return
@@ -56,6 +56,48 @@ public class _01_order_I {
             if (pop.left != null) {
                 stack.push(pop.left);
             }
+        }
+        return result;
+    }
+
+    /**
+     * 非递归：第二种写法
+     */
+    public List<Integer> preOrder_03(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                // 前序遍历原则：第一次碰到就打印
+                result.add(curr.val);
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                curr = curr.right;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 非递归：第三种写法
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preOrder_04(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                // 前序遍历原则：第一次碰到就打印
+                result.add(curr.val);
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            curr = curr.right;
         }
         return result;
     }
