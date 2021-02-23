5d0bf893a662cb84af6a56cab0b2787951c99282
sunjinwei <sunjinwei5828@163.com>
2021-02-23 08:53:54 +0800

    二叉树：序列化和反序列化，使用前序遍历，序列化ok，反序列化的错误方式，只使用数组是不行的，要用队列

---
+++ src/com/sunjinwei/tree/_03_serialize_deserialize.java
@@ -0,0 +1,84 @@
+package com.sunjinwei.tree;
+
+import com.sunjinwei.domain.TreeNode;
+
+/**
+ * 二叉树的序列化和反序列化
+ */
+public class _03_serialize_deserialize {
+
+    /**
+     * 序列化: 将二叉树序列化为一个字符串
+     * 遇到空节点，记为“#!”
+     * 如果不是空节点 获取到节点的val，拼接“!”
+     * !：代表一个节点的结束
+     *
+     * @param root
+     * @return
+     */
+    public String serialByPre(TreeNode root) {
+        if (root == null) {
+            return "#!";
+        }
+        return doSerializeByPre(root, "");
+    }
+
+    private String doSerializeByPre(TreeNode root, String s) {
+        if (root == null) {
+            return s + "#!";
+        }
+        s = s + root.val + "!";
+        s = doSerializeByPre(root.left, s);
+        s = doSerializeByPre(root.right, s);
+        return s;
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param s
+     */
+    public TreeNode deSerializeByPre(String s) {
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+        String[] arr = s.split("!");
+        return dodeSerializeByPre(arr, 0);
+    }
+
+    private TreeNode dodeSerializeByPre(String[] arr, int index) {
+        if (index == arr.length) {
+            return null;
+        }
+        if (arr[index].equals("#")) {
+            return null;
+        }
+        String s = arr[index];
+        TreeNode treeNode = new TreeNode(Integer.parseInt(s));
+        treeNode.left = dodeSerializeByPre(arr, index++);
+        treeNode.right = dodeSerializeByPre(arr, index++);
+        return treeNode;
+    }
+
+    public static void main(String[] args) {
+        _03_serialize_deserialize serializeDeserialize = new _03_serialize_deserialize();
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+
+        TreeNode left2 = new TreeNode(4);
+        TreeNode right3 = new TreeNode(5);
+
+        right.left = left2;
+        right.right = right3;
+
+
+        String s = serializeDeserialize.serialByPre(root);
+        System.out.println(s);
+
+        TreeNode treeNode = serializeDeserialize.deSerializeByPre(s);
+        System.out.println(treeNode.val);
+    }
+}
