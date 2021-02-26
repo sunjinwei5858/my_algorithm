2a7400e147f1950fdc4eb8d2449eec64d44e9cbb
sunjinwei <sunjinwei5828@163.com>
2021-02-26 09:06:25 +0800

    二叉树：序列化和反序列化：层序遍历实现反序列化

--- src/com/sunjinwei/tree/_03_serialize_deserialize_III.java
+++ src/com/sunjinwei/tree/_03_serialize_deserialize_III.java
@@ -7,11 +7,14 @@ import java.util.Queue;
 
 /**
  * 序列化和反序列化：层序遍历
+ * <p>
  */
 public class _03_serialize_deserialize_III {
 
     /**
-     * 序列化：层序遍历,方法一：空节点也进入队列
+     * 序列化,方法一：空节点也进入队列
+     * 执行用时：117 ms, 在所有 Java 提交中击败了18.09%的用户
+     * 内存消耗：40.6 MB, 在所有 Java 提交中击败了40.42%的用户
      */
     public String serialize(TreeNode root) {
         // 初始化队列
@@ -36,6 +39,9 @@ public class _03_serialize_deserialize_III {
 
     /**
      * 序列化：层序遍历,方法二：空节点不进入队列，提前拼接到字符串中
+     * <p>
+     * 执行用时：123 ms, 在所有 Java 提交中击败了17.85%的用户
+     * 内存消耗：40.9 MB, 在所有 Java 提交中击败了35.60%的用户
      */
     public String serialize_02(TreeNode root) {
         if (root == null) {
@@ -67,25 +73,38 @@ public class _03_serialize_deserialize_III {
      * 反序列化：
      */
     public TreeNode deserialize(String data) {
-        if (data == null || data == "") {
+        if (data == null || data.equals("#!")) {
             return null;
         }
         String[] split = data.split("!");
-        Queue<String> queue = new LinkedList<>();
-        for (String s : split) {
-            queue.offer(s);
-        }
-        String poll = queue.poll();
-        TreeNode root = new TreeNode(Integer.parseInt(poll));
+        int index = 0;
+        TreeNode root = transformString2TreeNode(split[index]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 队列记录父节点
+        queue.offer(root);
+        TreeNode node = null;
         while (!queue.isEmpty()) {
-            String s = queue.poll();
-            if (s != "#") {
-
-            } else {
-
+            // 队列中存放的都是父节点
+            node = queue.poll();
+            // 父节点对应左侧节点的值
+            node.left = transformString2TreeNode(split[++index]);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            // 父节点对应右侧节点的值
+            node.right = transformString2TreeNode(split[++index]);
+            if (node.right != null) {
+                queue.offer(node.right);
             }
         }
-        return new TreeNode(1);
+        return root;
+    }
+
+    private TreeNode transformString2TreeNode(String s) {
+        if (s.equals("#")) {
+            return null;
+        }
+        return new TreeNode(Integer.parseInt(s));
     }
 
     public static void main(String[] args) {
@@ -102,7 +121,11 @@ public class _03_serialize_deserialize_III {
         right.right = right3;
 
         _03_serialize_deserialize_III deserialize_iii = new _03_serialize_deserialize_III();
-        String serialize = deserialize_iii.serialize(root);
-        System.out.println(serialize);
+        String data = deserialize_iii.serialize(root);
+        System.out.println(data);
+        TreeNode deserialize = deserialize_iii.deserialize(data);
+        System.out.println(deserialize);
+
+
     }
 }
