7866f52bbae3f9c1f1c6a732f758f5919b5a0822
sunjinwei <sunjinwei5828@163.com>
2021-03-11 17:33:24 +0800

    feat: 二叉树的镜像，递归解法

---
+++ src/com/sunjinwei/tree/_10_mirror_tree.java
@@ -0,0 +1,50 @@
+package com.sunjinwei.tree;
+
+import com.sunjinwei.domain.TreeNode;
+
+/**
+ * 返回二叉树的镜像
+ */
+public class _10_mirror_tree {
+
+    /**
+     * 方法1：递归
+     * 思路：递归处理 开始是根节点 交换左右孩子，然后左子树进行交换 将大问题转换为小问题
+     * <p>
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.8 MB, 在所有 Java 提交中击败了59.46%的用户
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.right = left;
+        root.left = right;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        _10_mirror_tree mirror_tree = new _10_mirror_tree();
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+
+        TreeNode left1 = new TreeNode(4);
+        TreeNode right1 = new TreeNode(5);
+        left.left = left1;
+        left.right = right1;
+
+        TreeNode treeNode = mirror_tree.mirrorTree(root);
+        System.out.println(treeNode.val);
+    }
+
+}
