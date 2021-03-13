c9b986e2abfd5aa8ebcdd4a72c07ef33a36c68cc
sunjinwei <sunjinwei5828@163.com>
2021-03-13 18:46:33 +0800

    feat: 二叉树的序列化和反序列化，前序实现，代码的重构和加深理解这道题目

--- src/com/sunjinwei/tree/_03_serialize_deserialize_I.java
+++ src/com/sunjinwei/tree/_03_serialize_deserialize_I.java
@@ -2,87 +2,94 @@ package com.sunjinwei.tree;
 
 import com.sunjinwei.domain.TreeNode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 二叉树的序列化和反序列化: 前序遍历的方式
- * 序列化：直接使用前序遍历模板
- * 反序列化：使用单向队列即可，队列的第一个元素是根节点，然后处理左子树 然后处理右子树
+ * 二叉树的序列化和反序列化: 前序遍历
+ * 注意：
+ * 1.两个标记：空节点使用"#",每个节点都附加上"!"
+ * 2.单独抽取两个方法：a.将节点转为字符串 b.将字符串转为节点
  */
 public class _03_serialize_deserialize_I {
 
     /**
-     * 序列化: 将二叉树序列化为一个字符串
-     * 遇到空节点，记为“#!”
-     * 如果不是空节点 获取到节点的val，拼接“!”
-     * !：代表一个节点的结束
+     * 前序实现序列化
      *
      * @param root
      * @return
      */
-    public String serializeByPre(TreeNode root) {
+    public String serialize(TreeNode root) {
+        // 1鲁棒性
         if (root == null) {
-            return "#!";
+            return convertTreeNode2String(root);
         }
-        return doSerializeByPre(root, "");
-    }
-
-    private String doSerializeByPre(TreeNode root, String s) {
-        if (root == null) {
-            return s + "#!";
-        }
-        s = s + root.val + "!";
-        s = doSerializeByPre(root.left, s);
-        s = doSerializeByPre(root.right, s);
-        return s;
+        // 2根节点
+        String rootStr = convertTreeNode2String(root);
+        // 3左子树
+        rootStr += serialize(root.left);
+        // 4右子树
+        rootStr += serialize(root.right);
+        return rootStr;
     }
 
     /**
-     * 第二种方式：直接套用前序遍历模板
+     * 将节点变成字符串的方法
      *
-     * @param root
+     * @param node
      * @return
      */
-    private String doSerializeByPre_02(TreeNode root) {
-        if (root == null) {
+    private String convertTreeNode2String(TreeNode node) {
+        if (node == null) {
             return "#!";
         }
-        String s = root.val + "!";
-        s += doSerializeByPre_02(root.left);
-        s += doSerializeByPre_02(root.right);
-        return s;
+        return node.val + "!";
     }
 
     /**
-     * 反序列化：利用LinkedList队列将字符串转换为数组 再将数组换位队列进行存储 先进先出
+     * 将字符串变成节点的方法
+     *
+     * @param s
+     * @return
+     */
+    private TreeNode convertString2TreeNode(String s) {
+        if ("#".equals(s)) {
+            return null;
+        }
+        return new TreeNode(Integer.valueOf(s));
+    }
+
+    /**
+     * 前序实现反序列化：依靠队列实现
      *
      * @param data
      */
-    public TreeNode deSerializeByPre(String data) {
+    public TreeNode deserialize(String data) {
         if (data == null || data.length() == 0) {
             return null;
         }
         String[] arr = data.split("!");
-        // 将数组转换成队列存储
-        Queue<String> queue = new LinkedList<>();
-        for (String s1 : arr) {
-            queue.offer(s1);
-        }
-        return dodeSerializeByPre(queue);
+        // 将数组转换成队列存储 使用Arrays工具类的asList方法 使代码更加优雅
+        Queue<String> queue = new LinkedList<>(Arrays.asList(arr));
+        // 前序递归处理
+        return deserialize_help(queue);
     }
 
-    private TreeNode dodeSerializeByPre(Queue<String> queue) {
+    private TreeNode deserialize_help(Queue<String> queue) {
+        // 1终止条件1：队列为空
         if (queue.isEmpty()) {
             return null;
         }
-        String s = queue.poll();
-        if (s.equals("#")) {
+        TreeNode treeNode = convertString2TreeNode(queue.poll());
+        // 2终止条件2：如果是空节点 返回null即可
+        if (treeNode == null) {
             return null;
         }
-        TreeNode treeNode = new TreeNode(Integer.parseInt(s));
-        treeNode.left = dodeSerializeByPre(queue);
-        treeNode.right = dodeSerializeByPre(queue);
+        // 3左子树
+        treeNode.left = deserialize_help(queue);
+        // 4右子树
+        treeNode.right = deserialize_help(queue);
         return treeNode;
     }
 
@@ -101,10 +108,10 @@ public class _03_serialize_deserialize_I {
         right.right = right3;
 
 
-        String s = serializeDeserialize.serializeByPre(root);
+        String s = serializeDeserialize.serialize(root);
         System.out.println(s);
 
-        TreeNode treeNode = serializeDeserialize.deSerializeByPre(s);
+        TreeNode treeNode = serializeDeserialize.deserialize(s);
         System.out.println(treeNode.val);
     }
 }
