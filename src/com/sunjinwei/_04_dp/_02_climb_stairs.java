76dcb7b31d5076ca02318158bfafae9f1a11ae90
sunjinwei <sunjinwei5828@163.com>
2021-03-17 09:52:35 +0800

    feat(dp): dp的入门案例2：爬楼梯，动态规划经典方法/动态规划状态压缩方法

--- src/com/sunjinwei/_04_dp/_02_climb_stairs.java
+++ src/com/sunjinwei/_04_dp/_02_climb_stairs.java
@@ -10,38 +10,20 @@ package com.sunjinwei._04_dp;
 public class _02_climb_stairs {
 
     /**
-     * 动态规划求解，斐波那契数列
-     *
-     * @param n
-     * @return
-     */
-    public int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
-        }
-        int prev = 1;
-        int current = 2;
-
-        for (int i = 3; i < n; i++) {
-            int sum = prev + current;
-            prev = current;
-            current = sum;
-        }
-        return current;
-    }
-
-    /**
-     * 动态规划 一维数组求解
+     * 动态规划：经典方法求解
      *
      * @param n
      * @return
      */
     public int climbStairs_02(int n) {
-        // 这里必须是小于等于2 不是1 因为dp[2]=2 _04_dp[0]+_04_dp[1]=1
+        // 这里必须是小于等于2 不是1
+        // 当台阶为1时 只有1种方法 爬1个台阶
+        // 当台阶为2时 有2种方法 爬两次 爬一次
         if (n <= 2) {
             return n;
         }
         int[] dp = new int[n + 1];
+        // 初始化数据
         dp[0] = 0;
         dp[1] = 1;
 
@@ -49,6 +31,30 @@ public class _02_climb_stairs {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+    /**
+     * 动态规划：状态(空间)压缩求解
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        // base case
+        if (n <= 2) {
+            return n;
+        }
+        // 记录上一个值
+        int prev = 1;
+        // 当前值
+        int current = 2;
+        // 从第三个台阶开始
+        for (int i = 3; i < n; i++) {
+            int sum = prev + current;
+            prev = current;
+            current = sum;
+        }
+        return current;
     }
 
 
