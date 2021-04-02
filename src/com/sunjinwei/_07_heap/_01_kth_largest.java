5eb58d5a98cf1441010a2d9a54167c0b27552fd2
sunjinwei <sunjinwei5828@163.com>
2021-04-02 11:01:54 +0800

    feat(堆): 第k大元素，自己的写法，代码需要重构，逻辑太复杂

---
+++ src/com/sunjinwei/_07_heap/_01_kth_largest.java
@@ -0,0 +1,71 @@
+package com.sunjinwei._07_heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * 数据流中的第k大元素 力扣703
+ * <p>
+ * 设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第k大元素，不是第k个不同的元素。
+ * 请实现 KthLargest 类：
+ * <p>
+ * KthLargest(int k, int[] nums) 使用整数k和整数流nums初始化对象。
+ * int add(int val) 将val插入数据流nums后，返回当前数据流中第k大的元素。
+ * 题目数据保证，在查找第 k 大元素时，数组中至少有 k 个元素
+ * <p>
+ * 思路：
+ * 要找到第k大元素 那么直接使用二叉堆 java的优先级队列 堆顶放最小值
+ */
+public class _01_kth_largest {
+
+    private int k;
+
+    private int[] nums;
+
+    private PriorityQueue<Integer> priorityQueue;
+
+    public _01_kth_largest(int k, int[] nums) {
+        this.k = k;
+        this.nums = nums;
+        // 直接使用自然排序 堆顶存储最小值
+        this.priorityQueue = new PriorityQueue<>();
+        // 如果当前数组的长度小于k 那么直接添加到队列
+        if (nums.length < k) {
+            for (int i = 0; i < nums.length; i++) {
+                priorityQueue.offer(nums[i]);
+            }
+            return;
+        }
+        // 走到这里 说明长度大于k
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(nums[i]);
+        }
+        // 将数组第k个之后的元素进行处理
+        // 如果当前元素比堆顶最小值大 那么入队列
+        for (int i = k; i < nums.length; i++) {
+            if (priorityQueue.peek() < nums[i]) {
+                priorityQueue.poll();
+                priorityQueue.offer(nums[i]);
+            }
+        }
+    }
+
+    public int add(int val) {
+        int length = nums.length;
+        // 扩容
+        nums = new int[length + 1];
+        // 将val存储到数组
+        nums[length] = val;
+        if (length < k) {
+            priorityQueue.offer(val);
+            return priorityQueue.peek();
+        }
+        if (priorityQueue.peek() < val) {
+            priorityQueue.poll();
+            priorityQueue.offer(val);
+        }
+        // 第k大 也就是最小堆的堆顶 最小值
+        return priorityQueue.peek();
+
+    }
+
+}
