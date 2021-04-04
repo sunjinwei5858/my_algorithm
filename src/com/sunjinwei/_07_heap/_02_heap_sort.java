39c26bc410dd8281d934ca267a955d91ef7a3044
sunjinwei <sunjinwei5828@163.com>
2021-04-04 20:07:39 +0800

    feat(堆): 堆排序代码优化和时间复杂度分析

--- src/com/sunjinwei/_07_heap/_02_heap_sort.java
+++ src/com/sunjinwei/_07_heap/_02_heap_sort.java
@@ -16,15 +16,20 @@ public class _02_heap_sort {
      */
     public void heapSort(int[] arr) {
         // 第一步：构造大顶堆
-        heapInsert(arr);
+        // O(nlogn)
+        for (int i = 0; i < arr.length; i++) { // O(n)
+            heapInsert(arr, i); // O(logn)
+        }
         // 第二步：此时最顶部元素就是最大值 将它和数组最后一个元素进行交换 并且固定 调整剩余的元素
         int length = arr.length;
-        while (length > 1) {
+        // O(nlogn)
+        while (length > 1) {  // O(n)
             // 固定最大值: 此时数组的第一个索引为最大值 和最后一个元素进行交换
+            // O(1)
             swap(arr, 0, length - 1);
             // 此时固定最大值，size-- 数组最后一个元素不参与
             length--;
-            // 构造大顶堆
+            // 构造大顶堆 O(logn)
             heapify(arr, 0, length);
         }
     }
@@ -39,21 +44,23 @@ public class _02_heap_sort {
     private void heapify(int[] arr, int parentIndex, int length) {
         // 左孩子索引
         int left = 2 * parentIndex + 1;
-        // 右孩子索引
-        int right = 2 * parentIndex + 2;
         // 因为堆是完全二叉树 右孩子可以没有 但是左孩子一定要有
         while (left < length) {
-            // 如果父亲节点大于左右孩子 已经是大顶堆 退出循环
-            if (arr[parentIndex] > arr[left] && arr[parentIndex] > arr[right]) {
+            // 右孩子索引
+            int right = left + 1;
+            // 右孩子可能没有 所以需要先把判断右孩子索引放在前面
+            int largestIndex = ((right < length) && (arr[left] > arr[right])) ? left : right;
+            // 比较左右孩子较大者和父节点的值 哪个值更大 就取哪个索引
+            largestIndex = arr[parentIndex] > arr[largestIndex] ? parentIndex : largestIndex;
+            // 如果父节点确实比左右孩子的值大 那么退出循环
+            if (largestIndex == parentIndex) {
                 break;
             }
             // 否则交换元素
-            int largestIndex = ((arr[left] > arr[right]) && (right < length)) ? left : right;
             swap(arr, parentIndex, largestIndex);
             // 交换索引
             parentIndex = largestIndex;
             left = 2 * parentIndex + 1;
-            right = 2 * parentIndex + 2;
         }
 
     }
@@ -63,20 +70,18 @@ public class _02_heap_sort {
      *
      * @param arr
      */
-    private void heapInsert(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            // 当前索引
-            int curr = i;
-            // 父节点的索引
-            int parent = (curr - 1) / 2;
-            // 大顶堆条件：父节点的值大于当前索引的值
-            while (arr[parent] < arr[curr]) {
-                // 走到这里 说明需要交换
-                swap(arr, parent, curr);
-                // 更新索引
-                curr = parent;
-                parent = (curr - 1) / 2;
-            }
+    private void heapInsert(int[] arr, int index) {
+        // 当前索引
+        int curr = index;
+        // 父节点的索引
+        int parent = (index - 1) / 2;
+        // 大顶堆条件：父节点的值大于当前索引的值
+        while (arr[parent] < arr[curr]) {
+            // 走到这里 说明需要交换
+            swap(arr, parent, curr);
+            // 更新索引
+            curr = parent;
+            parent = (curr - 1) / 2;
         }
     }
 
