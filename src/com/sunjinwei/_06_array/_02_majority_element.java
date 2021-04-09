d8d8b2c60292beb94bdae83240bf0ef0a3d05948
sunjinwei <sunjinwei5828@163.com>
2021-04-09 09:40:21 +0800

    feat(数组)：数组中超过一半的数，【方法1：使用快排思路，但是可以提前终止递归】

--- src/com/sunjinwei/_06_array/_02_majority_element.java
+++ src/com/sunjinwei/_06_array/_02_majority_element.java
@@ -2,16 +2,16 @@ package com.sunjinwei._06_array;
 
 /**
  * 数组中超过一半的数
- * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。 剑指39
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。 剑指39 力扣169
  */
 public class _02_majority_element {
 
-    private int result;
-
     /**
-     * 思路：
-     * 荷兰国旗
-     * 超过一半 如果将数组进行排序 那么该数字必然在中间
+     * 方法1：使用快排，但是不需要把所有元素排序
+     * 超过一半 那么快排之后 中间位置的数肯定超过数组长度一半的数字
+     * <p>
+     * 执行用时：1 ms, 在所有 Java 提交中击败了99.98%的用户
+     * 内存消耗：41.6 MB, 在所有 Java 提交中击败了72.98%的用户
      *
      * @param nums
      * @return
@@ -21,31 +21,35 @@ public class _02_majority_element {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-
         quickSort(nums, 0, nums.length - 1);
-
-        return nums[(nums.length) / 2];
-
+        return nums[nums.length >> 1];
     }
 
     private void quickSort(int[] nums, int left, int right) {
 
+        // 终止条件1
         if (left >= right) {
             return;
         }
-
         int randomIndex = left + (int) (Math.random() * (right - left + 1));
-
         swap(nums, randomIndex, right);
-
         int[] pivotArr = partition(nums, left, right);
-
+        // 终止条件2：如果已经发现排序后中间的数在中间的化 那么停止递归
+        if (pivotArr[0] < (nums.length >> 1) && pivotArr[1] > (nums.length >> 1)) {
+            return;
+        }
         quickSort(nums, left, pivotArr[0] - 1);
-
         quickSort(nums, pivotArr[1] + 1, right);
-
     }
 
+    /**
+     * 荷兰国旗问题方法
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
     private int[] partition(int[] nums, int left, int right) {
         int less = left - 1;
         int more = right;
@@ -72,42 +76,11 @@ public class _02_majority_element {
         arr[i] = temp;
     }
 
-    public int majorityElement_02(int[] nums) {
-
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-
-        int right = nums.length - 1;
-
-        int middle = right >> 1;
-
-        int left = 0;
-
-        int randomIndex = left + (int) (Math.random() * (right - left + 1));
-
-        swap(nums, randomIndex, right);
-
-        int[] pivotArr = partition(nums, left, right);
-
-        while (pivotArr[1] < middle || pivotArr[0] > middle) {
-            if (pivotArr[0] > (nums.length / 2)) {
-                // 左移
-                pivotArr = partition(nums, left, pivotArr[0] - 1);
-            } else if (pivotArr[1] < middle) {
-                // 右移动
-                pivotArr = partition(nums, pivotArr[1] + 1, right);
-            }
-        }
-        return nums[(nums.length) / 2];
-
-    }
-
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2};
         _02_majority_element majorityElement = new _02_majority_element();
-        int element = majorityElement.majorityElement_02(arr);
+        int element = majorityElement.majorityElement(arr);
         System.out.println(element);
     }
 }
