caf807511686cc14deb38a11c3ba95167a60a4ab
sunjinwei <sunjinwei5828@163.com>
2021-04-24 17:06:39 +0800

    feat(数组): 两数之和的变种167 数组为有序数组

--- src/com/sunjinwei/test/_09_twoSum.java
+++ src/com/sunjinwei/test/_09_twoSum.java
@@ -9,11 +9,13 @@ import java.util.HashMap;
  * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  * 你可以按任意顺序返回答案。
+ * <p>
+ * 167两数之和变种II 输入有序数组
  */
 public class _09_twoSum {
 
     /**
-     * 使用哈希表
+     * 1 使用哈希表
      *
      * @param nums
      * @param target
@@ -28,6 +30,34 @@ public class _09_twoSum {
                 return new int[]{hashMap.get(target - nums[i]), i};
             }
             hashMap.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+
+    /**
+     * 167 先排序 后使用双指针，但是这样会改变元素的索引, 所以这样的做法的前提是数组有序
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int sum = 0;
+        // 双指针
+        while (left < right) {
+            sum = nums[left] + nums[right];
+            if (target == sum) {
+                return new int[]{left, right};
+            } else if (target > sum) {
+                left++;
+            } else {
+                right--;
+            }
+
         }
         return new int[]{-1, -1};
     }
