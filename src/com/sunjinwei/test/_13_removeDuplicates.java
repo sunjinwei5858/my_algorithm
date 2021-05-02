2b121d4f049b6f9cc93541b27d6f43d8209730d2
sunjinwei <sunjinwei5828@163.com>
2021-05-02 09:53:13 +0800

    feat(数组): 有序数组-删除重复项，使每个元素出现一次，双指针的while写法

--- src/com/sunjinwei/test/_13_removeDuplicates.java
+++ src/com/sunjinwei/test/_13_removeDuplicates.java
@@ -24,13 +24,48 @@ public class _13_removeDuplicates {
      */
     public int removeDuplicates(int[] nums) {
 
+        // 1定义结果数组的区间为[0,left] 左闭右闭
         int left = 0;
+        // 2因为数组本身是排好序的 所以令right指针为1即可
         for (int right = 1; right < nums.length; right++) {
+            // 如果left和right指向的值不想等 说明是需要将right放入结果数组[o,left]中的
             if (nums[left] != nums[right]) {
+                // left++ 是因为区间[o,slow]是左闭右闭的
                 left++;
                 nums[left] = nums[right];
             }
         }
+        // 3因为left是指向结果数组中最后一个元素的末尾
+        // 因为索引是从0开始 所以返回长度需要left+1
         return left + 1;
+    }
+
+
+    /**
+     * 方法2：使用while循环的快慢指针
+     *
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates2(int[] nums) {
+        // 1鲁棒性
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        // 2初始化快慢指针的索引!!!!
+        // 慢指针就是结果数组的区间[0,slow]
+        // 慢指针已经确定 与前面没有重复的元素的指针
+        int slow = 0;
+        // 快指针就是用来检测元素的指针
+        int fast = 1;
+        // 3处理
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
     }
 }
