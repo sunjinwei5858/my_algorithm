dcb4f7faee676bd6f3f432ebd9d4733a904faf6c
sunjinwei <sunjinwei5828@163.com>
2021-05-06 17:06:57 +0800

    feat(快排)：经典快排和荷兰问题快排，道理其实都是一样，可以分析出来，需要注意随机打乱!!!

---
+++ src/com/sunjinwei/_08_sort/_06_quick_sort_I.java
@@ -0,0 +1,95 @@
+package com.sunjinwei._08_sort;
+
+import java.util.Arrays;
+
+/**
+ * 快排：经典的做法 也需要进行打乱 不打乱 力扣也不通过 报超出时间限制!!!
+ * https://blog.csdn.net/zyd196504/article/details/87954669
+ * <p>
+ * 小于等于分界点pivot的数放左边，大于分界点pivot的放右边
+ * Partition方法返回一个int值
+ */
+public class _06_quick_sort_I {
+
+    public void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        /**
+         * 1随机打乱处理: 保证i在[left, right]的随机一个 这是为了概率分布
+         */
+        int i = left + (int) (Math.random() * (right - left + 1));
+        swap(arr, i, right);
+        /**
+         * 2开始partition
+         */
+        int pivot = partition(arr, left, right);
+        /**
+         * 3分析：
+         * 此时返回的pivot就是分界点 此时的数组 小于pivot的都在左边 大于pivot的都在右边
+         * 所以处理左边：[left, pivot-1], 处理右边[pivot+1, right]
+         */
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+
+    /**
+     * partition方法的思想：小于等于分界点的放左边，大于分界点的放右边
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private int partition(int[] arr, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        if (left == right) {
+            return left;
+        }
+        // 相当于慢指针 最后左边区间就是[0,less]
+        int less = left - 1;
+        // 遍历的索引i 从left开始 相当于快指针
+        int i = left;
+        // 默认使用right为分界点
+        while (i < right) {
+            /**
+             * 分支1：arr[i] <= 15，arr[i]和小于等于区的右边一个元素交换，同时小于等于区向右扩展1个，i++
+             * 分支2：arr[i] > 15，不做操作，只是i++
+             */
+            if (arr[i] <= arr[right]) {
+                less++;
+                swap(arr, less, i);
+            }
+            i++;
+        }
+        /**
+         * 处理：right分界点放置到正确的位置 也就是左边的区域，所以less需要++后才有位置
+         */
+        less++;
+        swap(arr, less, right);
+        return less;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        _06_quick_sort_I quickSortI = new _06_quick_sort_I();
+        int[] arr = new int[]{18, 15, 13, 17, 6, 20, 15, 9};
+        quickSortI.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/com/sunjinwei/_08_sort/_06_quick_sort_II.java
+++ src/com/sunjinwei/_08_sort/_06_quick_sort_II.java
@@ -3,17 +3,17 @@ package com.sunjinwei._08_sort;
 import java.util.Arrays;
 
 /**
- * 快排
+ * 快排 【荷兰国旗】
  * 分界点应该随机选取，而不是取最后一个或者中间点
  * 思路：有了分界点，就可以把数组通过荷兰国旗问题分成三个部分
  * 左侧：小于分界点
  * 中间值：等于分界点
  * 右侧：大于分界点
- *
+ * <p>
  * 这篇博客写的不错
  * https://blog.csdn.net/weixin_42507756/article/details/84444728?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control&dist_request_id=&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control
  */
-public class _06_quick_sort {
+public class _06_quick_sort_II {
 
     public void quickSort(int[] arr) {
         if (arr == null || arr.length < 2) {
@@ -34,11 +34,14 @@ public class _06_quick_sort {
         if (left >= right) {
             return;
         }
-        // 随机打乱arr[right]的值 这里使用arr[right]最后一个元素作为分界点
+        /**
+         * 随机打乱arr[right]的值 这里使用arr[right]最后一个元素作为分界点 保证快排的性能为nlogn
+         */
         int randomIndex = left + (int) (Math.random() * (right - left + 1));
         swap(arr, randomIndex, right);
+
         // 使用荷兰国旗寻找分界点范围数组
-        int[] pivotArr = partion(arr, left, right);
+        int[] pivotArr = partition(arr, left, right);
         quickSort(arr, left, pivotArr[0] - 1);
         quickSort(arr, pivotArr[1] + 1, right);
     }
@@ -51,27 +54,36 @@ public class _06_quick_sort {
      * @param right
      * @return
      */
-    private int[] partion(int[] arr, int left, int right) {
+    private int[] partition(int[] arr, int left, int right) {
         // 小于区
         int less = left - 1;
-        // 大于区: 因为先默认使用right元素作为分界点 保证了分界点在数组中 可以不需要more=right+1
+        // 大于区: 默认使用right元素作为分界点 保证了分界点在数组中
+        // 默认使用最右边元素为分界点
         int more = right;
         // 当前遍历的索引
-        int curr = left;
-        while (curr < more) {
-            // 当前元素比分界点小 继续放在小于区
-            if (arr[curr] < arr[right]) {
+        int index = left;
+        while (index < more) {
+            /**
+             * 小于区：arr[i]和小于区的下一个元素【less++】进行置换 index指针向前挪动一位
+             * 大于区：arr[i]和大于区的前一个元素【more--】进行置换 ps：index指针不需要向前挪动 因为不确定置换过来的数在哪个区
+             * 等于区：不需要操作 只需要index指针++即可
+             */
+            if (arr[index] < arr[right]) {
                 less++;
-                swap(arr, less, curr);
-                curr++;
-            } else if (arr[curr] > arr[right]) {
+                swap(arr, less, index);
+                index++;
+            } else if (arr[index] > arr[right]) {
                 more--;
-                swap(arr, more, curr);
+                swap(arr, more, index);
             } else {
-                curr++;
+                index++;
             }
         }
-        // 交换分界点
+        /**
+         * 处理：将right分界点放置在正确的位置 也就是扩大等于区，
+         * 此时more就是大于区的第一个，扩大等于区相当于挤压大于区，也就是占领more的位置
+         * 所以返回等于区的数组为{less+1, more}
+         */
         swap(arr, more, right);
         return new int[]{less + 1, more};
 
@@ -85,7 +97,7 @@ public class _06_quick_sort {
 
     public static void main(String[] args) {
 
-        _06_quick_sort _06_quick_sort = new _06_quick_sort();
+        _06_quick_sort_II _06_quick_sort = new _06_quick_sort_II();
         int[] arr = new int[]{2, 3, 1, 9, 7, 6, 1, 4, 5};
         _06_quick_sort.quickSort(arr);
         System.out.println(Arrays.toString(arr));
