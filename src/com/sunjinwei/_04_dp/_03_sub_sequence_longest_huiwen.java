12b8526ec8c3e34bbcdda656e2311f6d18c3c050
sunjinwei <sunjinwei5828@163.com>
2021-05-13 12:41:40 +0800

    feat(dp): 最长回文子串/最长回文子序列

---
+++ src/com/sunjinwei/_04_dp/_03_sub_sequence_longest_huiwen.java
@@ -0,0 +1,12 @@
+package com.sunjinwei._04_dp;
+
+/**
+ * 最长回文子序列 力扣
+ *
+ * 1返回长度
+ * 2返回这个子序列
+ */
+public class _03_sub_sequence_longest_huiwen {
+
+
+}
---
+++ src/com/sunjinwei/_12_huiwen/_01_huiwen_str_longest.java
@@ -0,0 +1,12 @@
+package com.sunjinwei._12_huiwen;
+
+/**
+ * 返回最长回文子串 力扣416
+ * 方法1：中心扩散法
+ * 方法2：动态规划
+ * 方法3：马拉车法
+ */
+public class _01_huiwen_str_longest {
+
+
+}
