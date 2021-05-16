9402264f9c334d531ab1b760c372d7358ba90551
sunjinwei <sunjinwei5828@163.com>
2021-05-16 12:39:30 +0800

    feat(dp): 最长公共子序列，暴力递归写法，和使用备忘录进行优化的写法!!!

--- src/com/sunjinwei/_04_dp/_03_sub_sequence_longest_common.java
+++ src/com/sunjinwei/_04_dp/_03_sub_sequence_longest_common.java
@@ -9,11 +9,15 @@ package com.sunjinwei._04_dp;
  * 最长公共子序列（Longest Common Subsequence，简称 LCS）是一道非常经典的面试题目，因为它的解法是典型的二维动态规划，
  * 大部分比较困难的字符串问题都和这个问题一个套路，比如说编辑距离。
  * 而且，这个算法稍加改造就可以用于解决其他问题，所以说 LCS 算法是值得掌握的。
+ * <p>
+ * 方法1：暴力递归
+ * 方法2：暴力递归+备忘录
+ * 方法3：动态规划
  */
 public class _03_sub_sequence_longest_common {
 
     /**
-     * 常规写法：
+     * 动态规划常规写法：
      * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
      *
      * @param text1
@@ -174,6 +178,80 @@ public class _03_sub_sequence_longest_common {
         }
         return dp;
     }
+
+
+    /**
+     * 暴力递归：力扣通不过 超出时间限制
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence4(String text1, String text2) {
+        // 鲁棒性1：只要其中有一个为空 那么返回0
+        if (text1 == null || text1.length() == 0) {
+            return 0;
+        }
+        if (text2 == null || text2.length() == 0) {
+            return 0;
+        }
+        // 辅助函数
+        return help(text1, text1.length() - 1, text2, text2.length() - 1);
+    }
+
+    private int help(String s1, int i, String s2, int j) {
+        if (i == -1 || j == -1) {
+            return 0;
+        }
+        if (s1.charAt(i) == s2.charAt(j)) {
+            // 需要+1
+            return help(s1, i - 1, s2, j - 1) + 1;
+        }
+        int res1 = help(s1, i - 1, s2, j);
+        int res2 = help(s1, i, s2, j - 1);
+        return Math.max(res1, res2);
+    }
+
+    /**
+     * 暴力递归优化：备忘录记忆化 【但是复杂度很高】
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    private Integer[][] arr;
+
+    public int longestCommonSubsequence5(String text1, String text2) {
+
+        // 鲁棒性1：只要其中有一个为空 那么返回0
+        if (text1 == null || text1.length() == 0) {
+            return 0;
+        }
+        if (text2 == null || text2.length() == 0) {
+            return 0;
+        }
+        arr = new Integer[text1.length()][text2.length()];
+        // 辅助函数
+        return help2(text1, text1.length() - 1, text2, text2.length() - 1);
+    }
+
+    private int help2(String s1, int i, String s2, int j) {
+        if (i == -1 || j == -1) {
+            return 0;
+        }
+        if (arr[i][j] != null) {
+            return arr[i][j];
+        }
+        if (s1.charAt(i) == s2.charAt(j)) {
+            arr[i][j] = help2(s1, i - 1, s2, j - 1) + 1;
+        } else {
+            int res1 = help2(s1, i, s2, j - 1);
+            int res2 = help2(s1, i - 1, s2, j);
+            arr[i][j] = Math.max(res1, res2);
+        }
+        return arr[i][j];
+    }
+
 
     public static void main(String[] args) {
         _03_sub_sequence_longest_common sequenceLongestCommon = new _03_sub_sequence_longest_common();
