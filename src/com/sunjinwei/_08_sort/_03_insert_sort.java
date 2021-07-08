3a6b6ad6f75ee456615b7f0443f8231761d7fc1b
sunjinwei <sunjinwei5828@163.com>
2021-07-08 10:47:10 +0800

    feat(排序)：数组的插入排序理解

--- src/com/sunjinwei/_08_sort/_03_insert_sort.java
+++ src/com/sunjinwei/_08_sort/_03_insert_sort.java
@@ -8,9 +8,18 @@ import java.util.Arrays;
  * 第一次[0,0]: 不需要进行比较
  * 第二次[0,1]: 从1开始看，[1]和[0]进行比较，如果更小，进行交换，然后再和i-1进行比较
  * 第三次[0,2]: 从2开始看，[2]和[1]进行比较，如果更小，进行交换，然后[1]和[0]进行比较
+ * <p>
+ * 比如已经排好序的数组为[1,4,5] 现在要插入3
+ * 那么让3和5进行比较，比5更小 那么进行交换，此时变成[1,4,3,5]，
+ * 继续往前进行比较，3和4进行比较，进行交换，此时变成[1,3,4,5]
  */
 public class _03_insert_sort {
 
+    /**
+     * 第一种写法
+     *
+     * @param arr
+     */
     public void insertSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -27,6 +36,27 @@ public class _03_insert_sort {
         }
     }
 
+    /**
+     * 第二种写法
+     *
+     * @param arr
+     */
+    public void insertSort2(int[] arr) {
+        // 鲁棒性：当数组只有一个元素时 不需要进行排序
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 外循环 直接从1开始
+        for (int i = 1; i < arr.length; i++) {
+            // 内循环不断的往前看
+            for (int j = i; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
     private void swap(int[] arr, int left, int right) {
         int temp = arr[right];
         arr[right] = arr[left];
@@ -36,7 +66,7 @@ public class _03_insert_sort {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 5, 4, 4};
         _03_insert_sort insertSort = new _03_insert_sort();
-        insertSort.insertSort(arr);
+        insertSort.insertSort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
