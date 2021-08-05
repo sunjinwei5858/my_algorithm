cfda3b96615d09383d95b60441b71ae42a01cfa2
sunjinwei <sunjinwei5828@163.com>
2021-08-05 19:13:44 +0800

    feat(dp)：买卖股票题型1，使用dp来处理

--- src/com/sunjinwei/_04_dp02/_04_Stock_Max_Profit_I.java
+++ src/com/sunjinwei/_04_dp02/_04_Stock_Max_Profit_I.java
@@ -37,6 +37,9 @@ public class _04_Stock_Max_Profit_I {
     /**
      * 解法2：贪心
      * 左边取最小，右边取最大，两者差值就是最大利润
+     * <p>
+     * 执行用时：3 ms, 在所有 Java 提交中击败了53.50%的用户
+     * 内存消耗：51.3 MB, 在所有 Java 提交中击败了61.27%的用户
      *
      * @param prices
      * @return
@@ -57,6 +60,51 @@ public class _04_Stock_Max_Profit_I {
             min = Math.min(min, prices[i]);
         }
         return max;
+    }
+
+    /**
+     * 解法3：动态规划
+     * <p>
+     * 执行用时：23 ms, 在所有 Java 提交中击败了13.35%的用户
+     * 内存消耗：55 MB, 在所有 Java 提交中击败了5.12%的用户
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit3(int[] prices) {
+
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        // 创建二维数组
+        int[][] dp = new int[prices.length][2];
+        // 状态定义dp[i][j]
+        // dp[i][0]: 表示第i天持有股票所得利润
+        // dp[i][1]: 表示第i天不持有股票所得利润
+
+        // 如何推持有股票和不持有股票所得利润
+        // 1 持有股票dp[i][0]
+        // 1.1 和i-1天保持一致 dp[i-1][0]
+        // 1.2 第i天持有股票 利润就是 -prices[i]
+        // 所以 最大利润就是max(dp[i-1][0], -prices[i])
+
+        // 2 不持有股票dp[i][1]
+        // 2.1 和i-1天保持一致 dp[i-1][1]
+        // 2.2 第i天卖出股票, 说明前一天肯定持有股票 prices[i]+dp[i-1][0]
+        // 所以 最大利润就是max(dp[i-1][1], prices[i]+dp[i-1][0])
+
+        // 初始化数据
+        // 第一天：持有股票0和不持有股票1
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            // 第i天 持有股票
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            // 第i天 不持有股票
+            dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+        }
+        return dp[prices.length - 1][1];
     }
 
 
