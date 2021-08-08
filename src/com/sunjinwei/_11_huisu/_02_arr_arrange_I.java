08a8753636e6ef133bb7a8c742aca6cd26d263ed
sunjinwei <sunjinwei5828@163.com>
2021-08-08 15:49:55 +0800

    feat(回溯)：重构 全排列 力扣46

--- src/com/sunjinwei/_11_huisu/_02_arr_arrange_I.java
+++ src/com/sunjinwei/_11_huisu/_02_arr_arrange_I.java
@@ -8,147 +8,83 @@ import java.util.List;
 /**
  * 全排列 回溯算法（dfs）：给定一个没有重复数字的序列，返回其所有可能的全排列。力扣46
  * <p>
- * 请大家做了一些回溯算法的问题以后顺便思考一下：深度优先遍历、递归、栈，它们三者的关系，
- * 我个人以为它们背后统一的逻辑都是「后进先出」。完成练习有助于我们深刻理解算法思想，我们加油！
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 这⾥和组合问题、切割问题和⼦集问题最⼤的不同就是for循环⾥不⽤startIndex了。
+ * 因为排列问题，每次都要从头开始搜索，例如元素1在[1,2]中已经使⽤过了，但是在[2,1]中还要再使⽤⼀次1。
+ * ⽽used数组，其实就是记录此时path⾥都有哪些元素使⽤了，⼀个排列⾥⼀个元素只能使⽤⼀次。
+ * 总结：
+ * ⼤家此时可以感受出排列问题的不同：
+ * 1每层都是从0开始搜索⽽不是startIndex
+ * 2需要used数组记录path⾥都放了哪些元素了
  */
 public class _02_arr_arrange_I {
 
     List<List<Integer>> result = new ArrayList<>();
 
-    public List<List<Integer>> permute(int[] nums) {
-        // 使用队列 移除最后一个元素比较方便
-        LinkedList<Integer> list = new LinkedList<>();
-        // 第一种方式
-        // huisu(nums, list);
-
-        // 第二种方式
-        //huisu_02(nums, list, 0, nums.length - 1);
-
-        // 第三种方式
-       /* boolean[] used = new boolean[nums.length];
-        huisu_03(nums, list, used);*/
-
-        // 第四种方式
-        boolean[] used04 = new boolean[nums.length];
-        huisu_04(nums, list, used04, 0);
-
-        return result;
-    }
-
     /**
      * 方法1：
      * 执行用时：2 ms, 在所有 Java 提交中击败了58.61%的用户
      * 内存消耗：38.6 MB, 在所有 Java 提交中击败了78.72%的用户
-     *
-     * @param nums
-     * @param list
      */
-    private void huisu(int[] nums, Deque<Integer> list) {
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> list = new LinkedList<>();
+        backTrack(nums, list);
+        return result;
+    }
+
+    private void backTrack(int[] nums, Deque<Integer> path) {
         // 递归终止条件
-        if (nums.length == list.size()) {
-            /**
-             * 在java中，参数传递是值传递，对象类型变量在传参的过程中，复制的是变量的地址。
-             * 这些地址被添加到res变量，但实际上指向的是同一块内存地址，因此我们会看到6个空的列表对象。
-             * 解决的方法很简单，在 res.addLast(path); 这里做一次拷贝即可。
-             *
-             */
-            result.add(new ArrayList<>(list));
+        if (nums.length == path.size()) {
+            result.add(new ArrayList<>(path));
             return;
         }
         for (int num : nums) {
             // 做选择 剪枝
-            if (list.contains(num)) {
+            if (path.contains(num)) {
                 continue;
             }
-            list.add(num);
-            huisu(nums, list);
+            path.add(num);
+            backTrack(nums, path);
             // 撤销选择 撤销的是list的最后一个元素
-            list.removeLast();
+            path.removeLast();
         }
     }
 
-    /**
-     * 方法二：自己想出来的方法
-     * 执行用时：2 ms, 在所有 Java 提交中击败了58.61%的用户
-     * 内存消耗：38.3 MB, 在所有 Java 提交中击败了96.93%的用户
-     *
-     * @param nums
-     * @param list
-     * @param start
-     * @param end
-     */
-    private void huisu_02(int[] nums, Deque<Integer> list, int start, int end) {
-        // 结束条件
-        if (list.size() == nums.length) {
-            result.add(new ArrayList<>(list));
-            return;
-        }
-        // 选择列表
-        for (int i = start; i <= end; i++) {
-            // 这里使用contains 不是很高效 需要O(N)的复杂度
-            if (list.contains(nums[i])) {
-                continue;
-            }
-            // 做选择
-            list.add(nums[i]);
-            // 递归
-            huisu_02(nums, list, 0, end);
-            // 撤销选择
-            list.removeLast();
-        }
-    }
 
     /**
-     * 方法三：题解中的 使用boolean数组保存是否使用的状态,进行递归的时候 将使用状态带进去 这样就不需要使用contains方法了，以空间换时间
+     * 方法2:
+     * 使用boolean数组保存是否使用的状态,进行递归的时候 将使用状态带进去 这样就不需要使用contains方法了，以空间换时间
      * 执行用时：2 ms, 在所有 Java 提交中击败了58.61%的用户
      * 内存消耗：38.7 MB, 在所有 Java 提交中击败了61.11%的用户
      */
-    private void huisu_03(int[] nums, LinkedList<Integer> list, boolean[] used) {
-        if (nums.length == list.size()) {
-            result.add(new ArrayList<>(list));
+    public List<List<Integer>> permute2(int[] nums) {
+        // 使用boolean数组
+        backTrack2(nums, new LinkedList<>(), new boolean[nums.length]);
+        return result;
+    }
+
+    private void backTrack2(int[] nums, LinkedList<Integer> path, boolean[] used) {
+        if (nums.length == path.size()) {
+            result.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                // 选择
-                list.add(nums[i]);
-                // 使用
-                used[i] = true;
-                // 递归
-                huisu_03(nums, list, used);
-                // 撤销使用
-                used[i] = false;
-                list.removeLast();
+            if (used[i]) {
+                continue;
             }
+            // 做选择
+            path.add(nums[i]);
+            // 使用
+            used[i] = true;
+            // 递归
+            backTrack2(nums, path, used);
+            // 撤销选择
+            used[i] = false;
+            path.removeLast();
         }
     }
-
-    /**
-     * 方法四：题解中的 使用boolean数组保存是否使用的状态,进行递归的时候 将使用状态带进去 这样就需要使用contains方法了，以空间换时间
-     * <p>
-     * 执行用时：1 ms, 在所有 Java 提交中击败了97.28%的用户
-     * 内存消耗：38.5 MB, 在所有 Java 提交中击败了79.86%的用户
-     */
-    private void huisu_04(int[] nums, LinkedList<Integer> list, boolean[] used, int start) {
-        if (nums.length == list.size()) {
-            result.add(new ArrayList<>(list));
-            return;
-        }
-        for (int i = start; i < nums.length; i++) {
-            if (!used[i]) {
-                // 选择
-                list.add(nums[i]);
-                // 使用
-                used[i] = true;
-                // 递归
-                huisu_04(nums, list, used, 0);
-                // 撤销使用
-                used[i] = false;
-                list.removeLast();
-            }
-        }
-    }
-
 
     public static void main(String[] args) {
         int[] ints = new int[]{1, 2, 3};
