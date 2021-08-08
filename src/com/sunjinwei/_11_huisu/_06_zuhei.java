f0b7dec14a46116c34d313c89a995cf4c35a1384
sunjinwei <sunjinwei5828@163.com>
2021-08-08 18:56:56 +0800

    feat(回溯)：组合优化，剪枝，非常好的剪枝手段!!!

--- src/com/sunjinwei/_11_huisu/_06_zuhei.java
+++ src/com/sunjinwei/_11_huisu/_06_zuhei.java
@@ -10,19 +10,20 @@ import java.util.List;
  * 示例:
  * 输入: n = 4, k = 2
  * 输出:
- * [
- * [2,4],
- * [3,4],
- * [2,3],
- * [1,2],
- * [1,3],
- * [1,4],
- * ]
+ * [[2,4],[3,4],[2,3],[1,2],[1,3],[1,4]]
  */
 public class _06_zuhei {
 
     private List<List<Integer>> result = new ArrayList<>();
 
+    /**
+     * 方法1：
+     * 执行用时：18 ms, 在所有 Java 提交中击败了58.71%的用户
+     * 内存消耗：39.4 MB, 在所有 Java 提交中击败了95.84%的用户
+     *
+     * @param n
+     * @param k
+     */
     public List<List<Integer>> combine(int n, int k) {
         // 声明放入元素的集合
         LinkedList path = new LinkedList<Integer>();
@@ -31,15 +32,6 @@ public class _06_zuhei {
         return result;
     }
 
-    /**
-     * 自己的解法， ac出来了
-     * 执行用时：18 ms, 在所有 Java 提交中击败了58.71%的用户
-     * 内存消耗：39.4 MB, 在所有 Java 提交中击败了95.84%的用户
-     * @param n
-     * @param k
-     * @param path
-     * @param start
-     */
     private void combine_help(int n, int k, LinkedList path, int start) {
         if (path.size() == k) {
             result.add(new ArrayList<>(path));
@@ -55,9 +47,42 @@ public class _06_zuhei {
         }
     }
 
+    /**
+     * 方法2：剪枝!!!!，比如 n = 5 ,k = 5 ,此时只需要遍历一次即可 不需要重复的递归处理了
+     * 执行用时：1 ms, 在所有 Java 提交中击败了99.98%的用户
+     * 内存消耗：39.8 MB, 在所有 Java 提交中击败了48.94%的用户
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> combine2(int n, int k) {
+        // 声明放入元素的集合
+        LinkedList path = new LinkedList<Integer>();
+        // 进行回溯
+        backTrack(n, k, path, 1);
+        return result;
+    }
+
+    private void backTrack(int n, int k, LinkedList path, int start) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 剪枝!!!! 极大的优化了时间复杂度
+        for (int i = 0; i <= n - (k - path.size()) + 1; i++) {
+            path.add(i);
+            backTrack(n, k, path, i + 1);
+            path.removeLast();
+        }
+
+    }
+
+
     public static void main(String[] args) {
         _06_zuhei zuhei = new _06_zuhei();
-        List<List<Integer>> combine = zuhei.combine(4, 2);
+        List<List<Integer>> combine = zuhei.combine(4, 4);
         for (List<Integer> integers : combine) {
             System.out.println(integers.toString());
         }
